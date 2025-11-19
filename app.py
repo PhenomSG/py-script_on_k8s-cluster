00e992559003bb06b6b9e3a776d7b878290c0ba8
sahaj <sahajguptarocks@gmail.com>
2025-11-19 12:05:37 +0530

    DS

--- app.py
+++ app.py
@@ -1,6 +1,2 @@
-num = 100
-if num % 2 == 0:
-    print("Even Number")
-    print("Hi, running py-script inside container is SUCCESSFUL")
-else:
-    print("Odd Number")
+print("PHENOMSG")
+print("Hi, running py-script inside container is SUCCESSFUL")
